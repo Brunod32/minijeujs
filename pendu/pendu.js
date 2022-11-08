addff48e03679c937a6411896bd6268bbc059979
Bruno Dahlem <bruno.dahlem2@gmail.com>
2022-11-08 14:53:14 +0100

    finish pendu

--- pendu/pendu.js
+++ pendu/pendu.js
@@ -1,46 +1,42 @@
-/*
-Todo:
-x Générer mot aléatoire
-X afficher le mot en masqué
-x pouvoir proposer des lettres
-x afficher lettres trouvées
-o Gérer la
-o gérer un nombre erreurs max
-*/
 import { Confetti } from "../lib/confetti.js";
 
+const allWords = [
+    "pokemon", "football", "peche",
+    "friends", "dessin", "danse", "metal",
+    "puzzle", "chocolat", "amour"
+];
 const buttonPlay = document.getElementById("beginGame");
-const allWords = ['ministre', 'montagne', 'constitution', 'corruption', 'economie', 'petrole'];
+const wordToFindDiv = document.getElementById("wordToFindDiv");
+const KeyBoardDiv= document.getElementById("KeyBoard");
+const cptErrorDiv= document.getElementById("errorCount");
+const hangedManImg = document.getElementById("hangedManImg");
 let wordToFind;
 let wordToFindArray;
-const wordToFindDiv = document.getElementById("wordToFindDiv");
-const keyBoardDiv = document.getElementById("keyBoard");
-const cptErrorDiv = document.getElementById("cptErreur");
-let cptErreur = 0;
-let cptFindedLetter = 0;
+let errorCount =0;
+let cptLettreTrouvees =0;
 
-buttonPlay.addEventListener("click", function() {
-    initGame();    
+buttonPlay.addEventListener("click", function(){
+    initGame();
 });
 
-function initGame() {
+function initGame(){
     Confetti.stopAnimationConfeti();
-    cptErreur = 0;
-    cptFindedLetter = 0;
+    errorCount = 0;
+    hangedManImg.className = '';
+    hangedManImg.classList.add("state"+errorCount);   
+    cptLettreTrouvees =0;
     wordToFindDiv.innerHTML = '';
     wordToFind = generateWord();
-
-    // Transmormer le mot en tableau
     wordToFindArray = Array.from(wordToFind);
     
-    let table = document.createElement("table");
+    let table = document.createElement("table"); 
     let line = document.createElement("tr");
-    line.id = "lineOfWord";
+    line.id="LineOfWord";
     wordToFindArray.forEach(letter => {
-        // Créer un td par lettres
+        //Créer un TD (case du tableau) par lettre
         let td = document.createElement("td");
         td.dataset.letter = letter;
-        td.innerHTML = "_";
+        td.innerText = "_";
         line.appendChild(td);
     });
 
@@ -48,74 +44,94 @@ function initGame() {
     wordToFindDiv.appendChild(table);
 
     generateKeyBoard();
-};
+}
 
-function generateKeyBoard() {
-    keyBoardDiv.innerHTML = '';
-    let alphabet = generateAlphabet();
-    alphabet.forEach(letter => {
-        let letterDiv = document.createElement("div");
-        letterDiv.innerHTML = letter;
-        letterDiv.classList.add("letterKeyBoard");
-        keyBoardDiv.appendChild(letterDiv);
+function generateKeyBoard(){
+    KeyBoardDiv.innerHTML = '';
+    let Alphabet = generateAlphabet();
+    Alphabet.forEach(letter => {
+        let lettreDiv =document.createElement("div");
+        lettreDiv.innerHTML = letter;
+        lettreDiv.classList.add("letterKeyBoard");
+        KeyBoardDiv.appendChild(lettreDiv);
 
-        letterDiv.addEventListener("click", () => {
-            if (checkLetterInWord(letter)) {
-               //Affiche la lettre dans le mot masqué
-                let lineOfWord = document.getElementById("lineOfWord");
-                let allTdOfWord = lineOfWord.children;
+        lettreDiv.addEventListener("click", () => {
+            if(checkLetterInWord(letter)){
+                //Afficher la lettre dans le mot masqué
+                let lineWord = document.getElementById("LineOfWord");
+                let allTdOfWord = lineWord.children;
                 Array.from(allTdOfWord).forEach(td => {
-                    if (td.dataset.letter == letter) {
+                    if(td.dataset.letter == letter){
                         td.innerHTML = letter;
-                        cptFindedLetter++;
+                        cptLettreTrouvees ++;
                     }
                 });
 
-                if (cptFindedLetter == wordToFindArray.length) {
-                    keyBoardDiv.innerHTML = '';
-                    cptErrorDiv.innerHTML = "Gagné, avec " + cptErreur + " erreurs";
+                if(cptLettreTrouvees == wordToFindArray.length){
+                    KeyBoardDiv.innerHTML = '';
+                    cptErrorDiv.innerHTML = 'Gagné, avec '+errorCount+' erreur(s)';
                     Confetti.launchAnimationConfeti();
-                    setTimeout(() => {
+                    setTimeout(() =>{
                         Confetti.stopAnimationConfeti();
                     }, 5000);
                 }
-            } else {
-                cptErreur++;
-                cptErrorDiv.innerHTML = cptErreur;
-                if (cptErreur >= 5) {
-                    cptErrorDiv.innerHTML = "Perdu avec 5 erreurs."
-                    let allTdOfWord = lineOfWord.children;
+            }
+            else{
+                //Incrémenter le compteur d'erreur
+                errorCount++;
+                cptErrorDiv.innerHTML = errorCount;
+                hangedManImg.className = '';
+                hangedManImg.classList.add("state"+errorCount);
+                if(errorCount >= 4){
+                    //On a perdu
+                    cptErrorDiv.innerHTML = "Perdu, vous avez fait plus de 4 erreurs.";
+                    let lineWord = document.getElementById("LineOfWord");
+                    let allTdOfWord = lineWord.children;
                     Array.from(allTdOfWord).forEach(td => {
                         td.innerHTML = td.dataset.letter;
-                    })
+                    });
+                    KeyBoardDiv.innerHTML = '';
                 }
             }
 
-            letterDiv.style.visibility = "hidden";
+            lettreDiv.style.visibility = "hidden";
         })
-    })
-};
-
-function generateAlphabet(capital = false) {
-    return [...Array(26)].map((_, i) => String.fromCharCode(i + (capital ? 65 : 97)));
+    });
 }
 
-function generateWord() {
+function generateAlphabet(capital = false) {
+	return [...Array(26)].map((_, i) => String.fromCharCode(i + (capital ? 65 : 97)));
+
+    // Ou
+    // let tab = [];
+    // let i = 65;
+    // if(!capital)
+    // {
+    //     i += 32;
+    // }
+    // let finish = i+26;
+    // for(i; i<finish; i++){
+    //     tab.push(String.fromCharCode(i));
+    // }
+    // return tab;
+}
+
+function generateWord(){
     let indexWord = getRandomInt(allWords.length);
     return allWords[indexWord];
-};
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
-};
+}
 
-// retourne true si lettre est présente dans le mot si absente retourne false
-function checkLetterInWord(letter) {
+//Retourne true si la lettre est présente dans le mot
+function checkLetterInWord(letter){
     let findLetter = false;
     wordToFindArray.forEach(letterOfWord => {
-        if (letter == letterOfWord) {
-            findLetter = true;
-        }
+        if(letter == letterOfWord){
+            findLetter =  true;
+        } 
     });
     return findLetter;
 }
